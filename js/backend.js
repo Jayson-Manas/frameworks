ae53bf8221fd807f5b09d6f23a97a1ea7e472c28
Jayson-Manas <gallowabdul@gmail.com>
2019-01-22 16:59:25 +0200

    input completed

--- js/backend.js
+++ js/backend.js
@@ -50,6 +50,27 @@
 
             }
 
+            function useInput1(randomNum){
+                document.getElementById('quote').innerHTML=randomNum;
+                newQuote(randomNum)
+            }
+
+            //Get a reference to the button
+                var button = document.getElementById("generate");
+
+                //Run the function on button click
+                function generate() {
+
+                // Create a variable to hold a random number between 1 and 100
+                var randomNum = Math.floor((Math.random() * 9999) + 1);
+
+                // Create a variable el to hold the ranNum element
+                var el = document.getElementById('ranNum');
+                // Write the number into that element
+                el.innerHTML = randomNum;
+
+                useInput1(randomNum);
+                }
             // $(document).ready(function () {
             //     newQuote();
             //     $('button').on('click', newQuote());
